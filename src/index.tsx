4334bc3a2f9d92040a3e6b9684c57922c8612758
clhilgert <claytonhilgertmst@gmail.com>
2023-08-14 21:56:53 -0500

    updated backend

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import store from './redux/store';
+import App from './components/App.jsx';
+import store from './redux/store.js';
 import { Provider } from 'react-redux';
-// import styles
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
 
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+document.addEventListener('DOMContentLoaded', function () {
+  const rootElement = document.getElementById('root');
+  
+  
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  } else {
+    console.log("Error")
+  }
+  
+  
+})
+
+
+
