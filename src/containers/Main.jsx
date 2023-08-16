0f6df32bf6a005dd4549627182b3b7aaaf9a80be
Chapsky <chapsky@Chapskys-MacBook-Pro.local>
2023-08-16 16:54:15 -0700

    added Navbar, Settings, and History components

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import NavBar from './navBar';
 
 export default function () {
-  return <div>Hello World</div>;
+  return <div>
+    {/* Hello World */}
+      <NavBar />
+    </div>;
 }
