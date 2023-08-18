ccadf4a69d2ec655a6960e097398c87c2dd2d267
Darren Pavel <darren.pavel@gmail.com>
2023-08-18 00:29:35 -0700

    A lot of CSS changes

--- src/reducers/chartSlice.js
+++ src/reducers/chartSlice.js
@@ -4,11 +4,12 @@ export const newChart = createAsyncThunk(
   'chart/newChart',
   async (_, thunkAPI) => {
     const state = thunkAPI.getState();
-    const count = 50;
-    const apiUrl = `http://localhost:8080/dummy/${count}`;
+    const count = 10;
+    const apiUrl = `http://localhost:8080/api/dummy/${count}`;
 
     const res = await fetch(apiUrl);
     const data = await res.json();
+    console.log(data);
 
     return data;
   }
