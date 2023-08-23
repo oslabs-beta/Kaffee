827ea18547b5513360c1a8100e8f467bee8ef210
clhilgert <claytonhilgertmst@gmail.com>
2023-08-23 18:05:16 -0500

    changed jmx port to 9092

--- java-server/server/src/main/java/com/kaffee/server/controllers/ServerMetricController.java
+++ java-server/server/src/main/java/com/kaffee/server/controllers/ServerMetricController.java
@@ -42,7 +42,7 @@ public class ServerMetricController {
 
   // Set defaults in the constructor
   public ServerMetricController() {
-    JMX_PORT = 9999;
+    JMX_PORT = 9092;
     SERVER_JMX_STRING = "service:jmx:rmi:///jndi/rmi://localhost:%d/jmxrmi";
     try {
       jmxServerMetrics = getServerMetricsStrings();
