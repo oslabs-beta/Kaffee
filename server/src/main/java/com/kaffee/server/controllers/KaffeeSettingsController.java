a731594566ac2bb63f5baa3813e3912da8d1272a
Duke Ahn <108208573+AhnDuke@users.noreply.github.com>
2023-10-27 17:15:23 -0700

    remove unused imports, fix return type

--- server/src/main/java/com/kaffee/server/controllers/KaffeeSettingsController.java
+++ server/src/main/java/com/kaffee/server/controllers/KaffeeSettingsController.java
@@ -2,17 +2,11 @@ package com.kaffee.server.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kaffee.server.UserSettings.ReadSettings;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -22,7 +16,7 @@ import java.nio.file.Paths;
 public class KaffeeSettingsController {
 //Get Settings
   @GetMapping("/getSettings")
-  private ResponseEntity getSettings() throws IOException{
+  private ResponseEntity<String> getSettings() throws IOException{
     String resourceName = "src/main/java/com/kaffee/server/settings.json";
     String stringified = new String(Files.readAllBytes(Paths.get(resourceName)));
     System.out.println(stringified);
