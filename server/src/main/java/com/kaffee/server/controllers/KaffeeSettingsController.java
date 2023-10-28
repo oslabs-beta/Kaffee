9d25d16ff5681c04623d3d504ba30dce94c05024
Duke Ahn <108208573+AhnDuke@users.noreply.github.com>
2023-10-27 17:24:11 -0700

    document and comment getSettings method

--- server/src/main/java/com/kaffee/server/controllers/KaffeeSettingsController.java
+++ server/src/main/java/com/kaffee/server/controllers/KaffeeSettingsController.java
@@ -14,12 +14,14 @@ import java.nio.file.Paths;
 @RestController
 @RequestMapping("/")
 public class KaffeeSettingsController {
-//Get Settings
+//Get Settings Route and Handler
   @GetMapping("/getSettings")
   private ResponseEntity<String> getSettings() throws IOException{
+    //Declare path to settings.json
     String resourceName = "src/main/java/com/kaffee/server/settings.json";
+    //Read all bytes as bytes[], then stringify using new String();
     String stringified = new String(Files.readAllBytes(Paths.get(resourceName)));
-    System.out.println(stringified);
+    //Return ResponseEntity with status 200 & body containing settings.json
     return ResponseEntity.ok(stringified);
   }
 }
