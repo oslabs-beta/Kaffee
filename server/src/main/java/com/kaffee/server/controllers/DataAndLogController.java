efcfe2233ab379c638dbabf5db0da82d87ea1c0e
Duke Ahn <108208573+AhnDuke@users.noreply.github.com>
2023-10-28 13:18:53 -0700

    updated getLogFiles method to return all files in Historical_logs

--- server/src/main/java/com/kaffee/server/controllers/DataAndLogController.java
+++ server/src/main/java/com/kaffee/server/controllers/DataAndLogController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.File;
+
 
 
 @RestController
@@ -15,9 +18,19 @@ import java.nio.file.Paths;
 public class DataAndLogController{
 //Get Historical Log Route
 @GetMapping("/getLogFiles")
-  private ResponseEntity<String> getLogFiles() throws IOException{
-    String resourceName = "/Users/lapduke/Desktop/Kaffee1.1/Kaffee/Historical_Logs/History.json";
-    String stringified = new String(Files.readAllBytes(Paths.get(resourceName)));
-    return ResponseEntity.ok(stringified);
+  private ResponseEntity<String[]> getLogFiles() throws IOException{
+    //directory path
+    String resourceName = "/Users/lapduke/Desktop/Kaffee1.1/Kaffee/Historical_Logs/";
+    //declare a new File at the directory path
+    File directory = new File(resourceName);
+    //declare a variable files which is an array of string of file names contained in the directory.
+    String files[] = directory.list();
+    //iterate through the files and list their name & their content using Files.readString
+    for(int i = 0; i<files.length; i++){
+      Path filePath = Paths.get("/Users/lapduke/Desktop/Kaffee1.1/Kaffee/Historical_Logs/",files[i]);
+      System.out.println(files[i] + ": " + "\n" + (Files.readString(filePath)));
+    }
+    //return the array of files
+    return ResponseEntity.ok(files);
   }
 }
