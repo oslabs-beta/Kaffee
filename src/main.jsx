aca3b668a706c29b205ed5abb7e17ff455f4960d
clhilgert <claytonhilgertmst@gmail.com>
2023-11-07 18:25:54 -0600

    remove unused Clusters component

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,6 @@ import './styles/style.css';
 
 import App from './App.jsx';
 import Main from './containers/Main.jsx';
-import Clusters from './containers/Clusters.jsx';
 import History from './containers/History.jsx';
 import Settings from './containers/Settings.jsx';
 import ErrorPage from './containers/ErrorPage.jsx';
@@ -22,12 +21,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} errorElement={<ErrorPage />}>
       <Route index element={<Main />} />
-      <Route path="clusters" element={<Clusters />} />
       <Route path="history" element={<History />} />
       <Route path="settings" element={<Settings />} />
       <Route path="about" element={<About />} />
-      <Route path="*" element={<ErrorPage />} />{' '}
-      <Route path="*" element={<ErrorPage />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
   )
